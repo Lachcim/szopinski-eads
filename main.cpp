a8a7bda014a6d28db47cea32cb99c4ce97a8faf7
Michał Szopiński <jajnick@hotmail.com>
2019-11-09 17:39:34 +0100

    Add template for final function

--- main.cpp
+++ main.cpp
@@ -4,3 +4,8 @@ int main()
 {
 
 }
+
+template <class Key, class Info>
+void split(Sequence<Key, Info> origin, int start, Sequence<Key, Info>& destination1, int step1, int count1, Sequence<Key, Info>& destination2, int step2, int count2) {
+
+}
