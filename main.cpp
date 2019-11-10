0068a78bce518d4161314a18a7a94b810c57a1ce
Michał Szopiński <jajnick@hotmail.com>
2019-11-10 20:25:08 +0100

    Aesthetic changes

--- main.cpp
+++ main.cpp
@@ -3,9 +3,9 @@
 #include "sequence.h"
 
 template <class Key, class Info>
-void split(Sequence<Key, Info> origin, int start, Sequence<Key, Info>& dest1, int step1, int count1, Sequence<Key, Info>& dest2, int step2, int count2) {
-    //obtain iterator of origin
-    typename Sequence<Key, Info>::iterator it = origin.begin();
+void split(Sequence<Key, Info> source, int start, Sequence<Key, Info>& dest1, int step1, int count1, Sequence<Key, Info>& dest2, int step2, int count2) {
+    //obtain iterator of source
+    typename Sequence<Key, Info>::iterator it = source.begin();
 
     //increment operator to start position
     for (int i = 0; i < start; i++)
@@ -18,11 +18,11 @@ void split(Sequence<Key, Info> origin, int start, Sequence<Key, Info>& dest1, in
         if (!outputtingTo1 && count2 == 0) continue;
 
         //copy step1 or step2 elements to dest1 or dest2
-        for (int i = 0; i < (outputtingTo1 ? step1 : step2) && it != origin.end(); i++)
+        for (int i = 0; i < (outputtingTo1 ? step1 : step2) && it != source.end(); i++)
             (outputtingTo1 ? dest1 : dest2).add(*it++);
 
-        //exit function if end of origin reached
-        if (it == origin.end())
+        //exit function if end of source reached
+        if (it == source.end())
             return;
 
         //decrement destination counter
@@ -32,21 +32,26 @@ void split(Sequence<Key, Info> origin, int start, Sequence<Key, Info>& dest1, in
 }
 
 void printSequence(Sequence<const char*, int>& sequence) {
+    //buffers for text storage
     char bufferTop[256];
     char bufferBottom[256];
     char bufferTopTemp[16];
     char bufferBottomTemp[16];
 
+    //labels
     strcpy(bufferTop, "Key:  ");
     strcpy(bufferBottom, "Info: ");
 
+    //add sequence data to buffers
     Sequence<const char*, int>::iterator i;
     for (i = sequence.begin(); i != sequence.end(); ++i) {
+        //write data to temp buffers
         sprintf(bufferTopTemp, "%s ", i->key);
         sprintf(bufferBottomTemp, "%d ", i->info);
 
         int lengthDifference = strlen(bufferTopTemp) - strlen(bufferBottomTemp);
 
+        //add spaces to achieve equal length
         if (lengthDifference < 0)
             for (int j = lengthDifference; j != 0; j++)
                 strcat(bufferTopTemp, " ");
@@ -54,12 +59,14 @@ void printSequence(Sequence<const char*, int>& sequence) {
             for (int j = lengthDifference; j != 0; j--)
                 strcat(bufferBottomTemp, " ");
 
+        //append temp buffers to final buffers
         strcat(bufferTop, bufferTopTemp);
         strcat(bufferBottom, bufferBottomTemp);
     }
 
+    //print buffers
     printf("%s\n", bufferTop);
-    printf("%s\n", bufferBottom);
+    printf("%s\n\n", bufferBottom);
 }
 int main()
 {
@@ -86,12 +93,12 @@ int main()
     Sequence<const char*, int> destination1;
     Sequence<const char*, int> destination2;
 
-    printf("\nSplitting: starting at 3, 2 elements to dest1 2 times, 3 elements to dest2 3 times\n");
+    printf("Splitting: starting at 3, 2 elements to dest1 2 times, 3 elements to dest2 3 times\n");
     split(source, 3, destination1, 2, 2, destination2, 3, 3);
 
     printf("Destination 1:\n");
     printSequence(destination1);
-    printf("\nDestination 2:\n");
+    printf("Destination 2:\n");
     printSequence(destination2);
 
     int i; scanf("%d", &i);
