57610ac0e9b986c2baea76509b388d73de0a0bff
Michał Szopiński <jajnick@hotmail.com>
2019-11-10 18:47:46 +0100

    Implement split function

--- main.cpp
+++ main.cpp
@@ -3,8 +3,32 @@
 #include "sequence.h"
 
 template <class Key, class Info>
-void split(Sequence<Key, Info> origin, int start, Sequence<Key, Info>& destination1, int step1, int count1, Sequence<Key, Info>& destination2, int step2, int count2) {
+void split(Sequence<Key, Info> origin, int start, Sequence<Key, Info>& dest1, int step1, int count1, Sequence<Key, Info>& dest2, int step2, int count2) {
+    //obtain iterator of origin
+    typename Sequence<Key, Info>::iterator it = origin.start();
 
+    //increment operator to start position
+    for (int i = 0; i < start; i++)
+        ++it;
+
+    //output to dest1 and dest2 interchangeably until both destinations exhausted
+    for (bool outputtingTo1 = true; count1 > 0 && count2 > 0; outputtingTo1 ^= true) {
+        //handle destination exhaustion
+        if (outputtingTo1 && count1 == 0) continue;
+        if (!outputtingTo1 && count2 == 0) continue;
+
+        //copy step1 or step2 elements to dest1 or dest2
+        for (int i = 0; i < (outputtingTo1 ? step1 : step2) && it != origin.end(); i++)
+            (outputtingTo1 ? dest1 : dest2).add(*it++);
+
+        //exit function if end of origin reached
+        if (it == origin.end())
+            return;
+
+        //decrement destination counter
+        if (outputtingTo1) count1--;
+        else count2--;
+    }
 }
 
 void printSequence(Sequence<const char*, int>& sequence) {
